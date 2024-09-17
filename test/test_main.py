fc93df7faa0797f5a8994f910a9f2a0ddbb970a8
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-17 12:15:17 -0300

    drop test files

--- test/test_main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from app.routes import game, player
-
-app_test = FastAPI()
-
-app_test.include_router(game.router, prefix="/game")
-app_test.include_router(player.router, prefix="/player")
