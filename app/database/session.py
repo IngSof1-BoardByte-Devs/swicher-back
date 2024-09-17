65b81d2301f5ad7649357edceddb7819b899cbbf
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-17 12:12:14 -0300

    drop test

--- app/database/session.py
+++ app/database/session.py
@@ -8,11 +8,11 @@ from pony.orm import Database
 # Crear instancia de la base de datos
 db = Database()
 
-# Esta función sirve para diferenciar entre la base de datos de producción y los tests
-def config_database(test: bool):
-    if test:
-        db.bind(provider="sqlite", filename="test_el_switcher_db.sqlite", create_db=True)
-    else:
-        db.bind(provider="sqlite", filename="el_switcher_db.sqlite", create_db=True)
-
+def config_database():
+    """
+    Configura la base de datos y genera las tablas si no existen.
+    """
+    # Conectar a la base de datos SQLite
+    db.bind(provider="sqlite", filename="el_switcher_db.sqlite", create_db=True)
+    
     db.generate_mapping(create_tables=True)
