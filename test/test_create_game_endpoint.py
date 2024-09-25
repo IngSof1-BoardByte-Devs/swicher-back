4d72c7a1b3238bbf8ea2aeb6122aeb343143e694
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-25 12:33:57 -0300

    correcciones en los test

--- test/test_create_game_endpoint.py
+++ test/test_create_game_endpoint.py
@@ -1,28 +1,39 @@
-import pytest
-from fastapi.testclient import TestClient
-from app.main import app
 from app.schemas.game import CreateGame
-from app.services.game import GameService
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from app.main import app
+from app.schemas.game import GameOut
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from app.database.models import Base, Game
 
-@pytest.fixture
-def client():
-    return TestClient(app)
+client = TestClient(app)
 
-def mock_create_game_service(self, game_data):
+# Fixture para la sesión de la base de datos en memoria
+@pytest.fixture(scope="module")
+def db_session() -> Session:
+    engine = create_engine('sqlite:///:memory:')  # Crea una base de datos SQLite en memoria
+    Base.metadata.create_all(engine)  # Crea las tablas en la base de datos
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+    yield session  # Esta es la sesión que tus pruebas usarán
+    session.close()
+
+# Función de simulación de la operación de la base de datos
+def mock_create_game_service(*args, **kwargs):
     # Mockear la respuesta del servicio
     return {"status": "OK", "game_id": 1}
 
-def test_create_game_endpoint(client, monkeypatch):
+def test_create_game_endpoint(db_session):
     # Stubbing: Crear datos para el usuario
     game_data = CreateGame(player_name="Juan", game_name="Mi juego")
 
     # Mocking: Mockear el servicio
-    monkeypatch.setattr(GameService, "create_game", mock_create_game_service)
-
-    # Enviar solicitud al endpoint
-    response = client.post("/game/create-game", json=game_data.dict())
+    with patch('app.services.game.GameService.create_game', new=mock_create_game_service):
+        # Enviar solicitud al endpoint
+        response = client.post("/game/create-game", json=game_data.model_dump())
 
     # Verificar respuesta
     assert response.status_code == 200
-    assert response.json()[0] == {"message": "Partida creada con éxito"}
-    print(response.json())
+    assert response.json() == [{"message": "Partida creada con éxito"}, 200]
--- test/test_join_game_endpoint.py
+++ test/test_join_game_endpoint.py
@@ -1,26 +1,63 @@
-import pytest
-from app.services.game import JoinGameService
 from app.schemas.game import JoinGame
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from app.main import app
+from app.schemas.game import GameOut
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from app.database.models import Base, Game
 
-@pytest.fixture
-def join_game_service():
-    return JoinGameService()
+client = TestClient(app)
+
+# Fixture para la sesión de la base de datos en memoria
+@pytest.fixture(scope="module")
+def db_session() -> Session:
+    engine = create_engine('sqlite:///:memory:')  # Crea una base de datos SQLite en memoria
+    Base.metadata.create_all(engine)  # Crea las tablas en la base de datos
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+    yield session  # Esta es la sesión que tus pruebas usarán
+    session.close()
+
+# Funciones de simulación de las operaciones de la base de datos
+def mock_get_game_by_id(*args, **kwargs):
+    game_id = kwargs.get('game_id', None)
+    if game_id == 1:
+        return Game(id=1, name="Juego 1")
+
+def mock_create_player(*args, **kwargs):
+    username = kwargs.get('username', None)
+    game = kwargs.get('game', None)
+    return {"username": username, "game": game}
 
 
-def test_join_game_success(join_game_service):
+
+# Test para unirse a un juego con éxito
+def test_join_game_success(db_session):
     game_data = JoinGame(game_id=1, player_name="Jugador 1")
-    response = join_game_service.join_game(game_data)
-    assert response["status"] == "OK"
-    assert response["message"] == "Jugador unido a la partida con éxito"
 
+    # Simular la operación de la base de datos, en vez de ejecutarse las operaciones crud se remplazan por las funciones de simulación
+    with patch('app.services.game.get_game', new=mock_get_game_by_id), \
+        patch('app.services.game.create_player', new=mock_create_player):
+        response = client.post("/game/join-game", json=game_data.model_dump())
+        assert response.status_code == 200
+        assert response.json() == [{'status': 'OK'}, 201]
 
-def test_join_game_invalid_game_id(join_game_service):
+# Test para unirse a un juego con un ID de juego inválido
+def test_join_game_invalid_game_id(db_session):
     game_data = JoinGame(game_id=-1, player_name="Jugador 1")
-    with pytest.raises(ValueError):
-        join_game_service.join_game(game_data)
 
+    with patch('app.database.crud.get_game', return_value=None):
+        response = client.post("/game/join-game", json=game_data.model_dump())
+        assert response.status_code == 400
+        assert response.json() == {'detail': {'message': 'ID de juego inválido', 'status': 'ERROR'}}
 
-def test_join_game_empty_player_name(join_game_service):
+# Test para unirse a un juego con un nombre de jugador vacío
+def test_join_game_empty_player_name(db_session):
     game_data = JoinGame(game_id=1, player_name="")
-    with pytest.raises(ValueError):
-        join_game_service.join_game(game_data)
+
+    with patch('app.database.crud.get_game', new=mock_get_game_by_id):
+        response = client.post("/game/join-game", json=game_data.model_dump())
+        assert response.status_code == 400
+        assert response.json() == {'detail': {'message': 'Nombre de jugador requerido', 'status': 'ERROR'}}
