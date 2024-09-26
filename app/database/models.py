096edac6c7f8b8b8de9ec5188f8590b6bd87891f
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-26 17:16:38 -0300

    update models

--- app/database/models.py
+++ app/database/models.py
@@ -1,53 +1,73 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.schema import CheckConstraint
+from app.utils.enums import *
 
 Base = declarative_base()
 
 class Game(Base):
     __tablename__ = 'games'
+    
+    # Atributos internos
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    players = relationship("Player", back_populates="game", foreign_keys='Player.game_id')  # Relación con players, usando game_id como clave foránea
     started = Column(Boolean, default=False)
     turn = Column(Integer, default=0)    
-    host = relationship("Player", uselist=False, back_populates="host_game", foreign_keys='Player.host_game_id')    # Relación con host, usando host_game_id como clave foránea
-        # tablero de enteros de 6x6
-    board = ARRAY(ARRAY(Integer))
+    board = Column(ARRAY(Integer), default=None)
+    
+    # Relaciones
+    players = relationship("Player", back_populates="game", foreign_keys='Player.game_id')
+    host = relationship("Player", uselist=False, back_populates="host_game", foreign_keys='Player.host_game_id')
+    
+    __table_args__ = (
+        CheckConstraint('array_length(board, 1) = 36', name='check_board_length'),
+    )
+
 
 class Player(Base):
     __tablename__ = 'players'
+    
+    # Atributos internos
     id = Column(Integer, primary_key=True)
-    username = Column(String)   
-    game_id = Column(Integer, ForeignKey('games.id'))   # Clave foránea a la tabla Game para la relación general (game_id)
-    game = relationship("Game", back_populates="players", foreign_keys=[game_id])    
-    host_game_id = Column(Integer, ForeignKey('games.id'))  # Clave foránea para la relación host (host_game_id)
-    host_game = relationship("Game", back_populates="host", foreign_keys=[host_game_id])
+    username = Column(String, nullable=False)
+    game_id = Column(Integer, ForeignKey('games.id', ondelete="CASCADE"), nullable=False)
+    host_game_id = Column(Integer, ForeignKey('games.id'))
     turn = Column(Integer, default=0)
-    movements = relationship("Movement", back_populates="player", foreign_keys='Movement.player_id')
+    
+    # Relaciones
+    game = relationship("Game", back_populates="players", foreign_keys=[game_id])    
+    host_game = relationship("Game", back_populates="host", foreign_keys=[host_game_id])
+    movements = relationship("Movement", back_populates="player", foreign_keys='Movement.player_id', lazy="dynamic")
     figures = relationship("Figure", back_populates="player", foreign_keys='Figure.player_id')
+
 
 class Movement(Base):
     __tablename__ = 'movements'
-    # Atributos de los movimientos
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
-    description = Column(String)
-    # Relación con player y game
-    player_id = Column(Integer, ForeignKey('players.id'))
-    player = relationship("Player", back_populates="movements", foreign_keys=[player_id])
-    game_id = Column(Integer, ForeignKey('games.id'))
-    game = relationship("Game", back_populates="movements", foreign_keys=[game_id])
     
+    # Atributos internos
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MovementType))
+    status = Column(Enum(MovementStatus), default=MovementStatus.INDECK)
+    
+    # Relaciones
+    player_id = Column(Integer, ForeignKey('players.id', ondelete='SET NULL'), nullable=True)
+    player = relationship("Player", back_populates="movements", foreign_keys=[player_id])
+    game_id = Column(Integer, ForeignKey('games.id', ondelete="CASCADE"), nullable=False)
+    game = relationship("Game", back_populates="movements", foreign_keys=[game_id])
+
+
 class Figure(Base):
     __tablename__ = 'figures'
-    # Atributos de las figuras
+    
+    # Atributos internos
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
-    # Relación con player y game
-    player_id = Column(Integer, ForeignKey('players.id'))
+    type = Column(Enum(FigureType))
+    status = Column(Enum(FigureStatus), default=FigureStatus.INDECK)
+    
+    # Relaciones
+    player_id = Column(Integer, ForeignKey('players.id'), nullable=False)
     player = relationship("Player", back_populates="figures", foreign_keys=[player_id])
-    game_id = Column(Integer, ForeignKey('games.id'))
+    game_id = Column(Integer, ForeignKey('games.id', ondelete="CASCADE"), nullable=False)
     game = relationship("Game", back_populates="figures", foreign_keys=[game_id])
