4eb9a9f66e7d05e2714b43e17d2f5e376fbf8018
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-26 14:06:44 -0300

    update last commits in develop

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
 
@@ -12,6 +13,8 @@ class Game(Base):
     started = Column(Boolean, default=False)
     turn = Column(Integer, default=0)    
     host = relationship("Player", uselist=False, back_populates="host_game", foreign_keys='Player.host_game_id')    # Relación con host, usando host_game_id como clave foránea
+        # tablero de enteros de 6x6
+    board = ARRAY(ARRAY(Integer))
 
 class Player(Base):
     __tablename__ = 'players'
@@ -22,3 +25,29 @@ class Player(Base):
     host_game_id = Column(Integer, ForeignKey('games.id'))  # Clave foránea para la relación host (host_game_id)
     host_game = relationship("Game", back_populates="host", foreign_keys=[host_game_id])
     turn = Column(Integer, default=0)
+    movements = relationship("Movement", back_populates="player", foreign_keys='Movement.player_id')
+    figures = relationship("Figure", back_populates="player", foreign_keys='Figure.player_id')
+
+class Movement(Base):
+    __tablename__ = 'movements'
+    # Atributos de los movimientos
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+    description = Column(String)
+    # Relación con player y game
+    player_id = Column(Integer, ForeignKey('players.id'))
+    player = relationship("Player", back_populates="movements", foreign_keys=[player_id])
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game", back_populates="movements", foreign_keys=[game_id])
+    
+class Figure(Base):
+    __tablename__ = 'figures'
+    # Atributos de las figuras
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    # Relación con player y game
+    player_id = Column(Integer, ForeignKey('players.id'))
+    player = relationship("Player", back_populates="figures", foreign_keys=[player_id])
+    game_id = Column(Integer, ForeignKey('games.id'))
+    game = relationship("Game", back_populates="figures", foreign_keys=[game_id])
