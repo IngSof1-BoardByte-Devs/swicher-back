463ecbc685432340fc59380eb58ed8c435a41e86
Daniela Agustina Courel <102161481+DanielaCourel@users.noreply.github.com>
2024-09-27 12:25:41 -0300

    Update crud.py

--- app/database/crud.py
+++ app/database/crud.py
@@ -24,14 +24,16 @@ def fetch_games(db: Session):
 
 def put_host(db: Session, game: Game, player: Player):
     game.host = player
-    db.commit()
-    
-@db_session
+
 def delete_player(player: Player, game: Game):
     game.players.remove(player)
     player.delete()
-""" 
-Usar @db_session cuando toda la función realiza operaciones con la base de datos, simplificando la gestión 
+
+def get_game_by_id(db: Session, game_id: int):  
+    return db.query(Game).filter(Game.id == game_id).first()
+
+def get_player_by_id(db: Session, player_id: int):
+    return db.query(Player).filter(Player.id == player_id).first()
 de la sesión.
 Usar with db_session cuando solo una parte de la función necesita acceder a la base de datos o cuando tienes 
 lógica compleja y necesitas más control sobre el manejo de la sesión. 
