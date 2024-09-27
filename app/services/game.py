a431b81a4a03042bab2d7c943f6e2438701148bf
Daniela Agustina Courel <102161481+DanielaCourel@users.noreply.github.com>
2024-09-27 12:27:38 -0300

    Update services/game.py

--- app/services/game.py
+++ app/services/game.py
@@ -4,7 +4,7 @@ Este archivo se encarga de manejar la lógica de negocio de la aplicación. Es d
 funciones que realizan operaciones más complejas y que no están directamente relacionadas con la base de datos.
 """
 
-from app.database.crud import fetch_games, leave_game
+from app.database.crud import *
 from app.database.models import Game, Player
 from app.schemas.game import GameOut
 from typing import List
@@ -18,8 +18,13 @@ class GameService:
         games = fetch_games(self.db)  # Aquí se pasa la sesión a la operación de la base de datos
         return [GameOut(id=g.id, name=g.name, num_players=len(g.players)) for g in games]
     
+    # Si no se encuentra el jugador o el juego, se lanza una excepción 400
     def leave_game(self, player_id: int, game_id: int):
-        player = Player[player_id]
-        game = Game[game_id]
-        if player in game.players:
-            leave_game(player, game)
+        player = get_player_by_id(self.db, player_id)
+        game = get_game_by_id(self.db, game_id)
+        if not player or not game or player not in game.players:
+            print("acá debería entrar")
+            raise Exception("Invalid player or game")
+        else:
+            print("acá no debería entrar")
+            delete_player(player, game)
