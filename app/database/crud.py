2543d628a22356e5315fa5e466d23a4de3133531
Daniela Agustina Courel <102161481+DanielaCourel@users.noreply.github.com>
2024-09-27 13:10:15 -0300

    SCRUM-9 fix crud delete player

--- app/database/crud.py
+++ app/database/crud.py
@@ -26,8 +26,11 @@ def put_host(db: Session, game: Game, player: Player):
     game.host = player
 
 def delete_player(player: Player, game: Game):
-    game.players.remove(player)
-    player.delete()
+    if not game.started:
+        raise Exception("Cannot leave game that has started")
+    else:
+        game.players.remove(player)
+        player.delete()
 
 def get_game_by_id(db: Session, game_id: int):  
     return db.query(Game).filter(Game.id == game_id).first()
