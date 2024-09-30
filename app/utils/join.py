89eb38b79802042c0bd28225d7bf439b0102bf55
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-09-30 11:57:48 -0300

    SCRUM-58 fix

--- app/utils/join.py
+++ app/utils/join.py
@@ -6,12 +6,12 @@ from fastapi import WebSocket
 def join_conecction(db: Session, game_id: int, ws: WebSocket):
     game = get_game(db, game_id)
     if game is None:
-        json_ws = {"event": "error", "data": {"message": "Game not found"}}
+        json_ws = {"event": "join_ws", "data": {"message": "Game not found", "status": 0}}
         ws.send_text(json.dumps(json_ws))
     elif game.started:
         json_ws = {"event": "join_ws", "data": {"message": "Game already started", "status": 1}}
         ws.send_text(json.dumps(json_ws))
     else:
-        json_ws = {"event": "join_ws", "data": {"message": "You have joined the game", "status": 0}}
+        json_ws = {"event": "join_ws", "data": {"message": "You have joined the game", "status": 2}}
         ws.send_text(json.dumps(json_ws))
     return False
