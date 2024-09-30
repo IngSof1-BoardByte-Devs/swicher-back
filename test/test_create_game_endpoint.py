72f0f8f1cbf4087824d77641c131386e3e29acc1
Franco Bustos <franco.bustos@mi.unc.edu.ar>
2024-09-30 18:11:33 -0300

    SCRUM-94 try (failed) fix test

--- test/test_create_game_endpoint.py
+++ test/test_create_game_endpoint.py
@@ -1,4 +1,4 @@
-from app.schemas.game import CreateGame
+from app.schemas.game import CreateGame, GameLeaveCreateResponse
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 from app.main import app
@@ -12,7 +12,7 @@ client = TestClient(app)
 
 # Fixture para la sesión de la base de datos en memoria
 @pytest.fixture(scope="module")
-def db_session() -> Session:
+def db_session():
     engine = create_engine('sqlite:///:memory:')  # Crea una base de datos SQLite en memoria
     Base.metadata.create_all(engine)  # Crea las tablas en la base de datos
     SessionLocal = sessionmaker(bind=engine)
@@ -21,9 +21,9 @@ def db_session() -> Session:
     session.close()
 
 # Función de simulación de la operación de la base de datos
-def mock_create_game_service(*args, **kwargs):
+def mock_create_game_service(*args, **kwargs) -> GameLeaveCreateResponse:
     # Mockear la respuesta del servicio
-    return {"status": "OK", "game_id": 1}
+    return GameLeaveCreateResponse(player_id=1,game_id=1)
 
 def test_create_game_endpoint(db_session):
     # Stubbing: Crear datos para el usuario
@@ -32,8 +32,9 @@ def test_create_game_endpoint(db_session):
     # Mocking: Mockear el servicio
     with patch('app.services.game.GameService.create_game', new=mock_create_game_service):
         # Enviar solicitud al endpoint
-        response = client.post("/game/create-game", json=game_data.model_dump())
+        response = client.post("/games/", json=game_data.model_dump())
+        print("Response: ", response)
 
     # Verificar respuesta
     assert response.status_code == 200
-    assert response.json() == [{"message": "Partida creada con éxito"}, 200]
+    assert response.json() == GameLeaveCreateResponse(player_id=1, game_id=1).model_dump()
