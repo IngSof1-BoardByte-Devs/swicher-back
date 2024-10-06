3fa120a6b060138f20f71bd419f59cbfb0365c26
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-10-05 22:37:46 -0300

    feature-ING-146 correcciones

--- app/services/figures.py
+++ app/services/figures.py
@@ -18,34 +18,38 @@ class FigureService:
         # Agrego las figuras simples al mazo
         easyTypes = [member for name, member in FigureType.__members__.items() if name.startswith('ETYPE')]
         for i in range(len(game.players) * 7):
-            figure_type = random.choice(easyTypes)
+            figure_type = easyTypes[i % len(easyTypes)]
             figure = new_figure(self.db, figure_type, game)
             deck.append(figure)
 
         # Agrego las figuras complejas al mazo
         complexTypes = list(member for name, member in FigureType.__members__.items() if name.startswith('TYPE'))
         for i in range(len(game.players) * 18):
-            figure_type = random.choice(complexTypes)
+            figure_type = complexTypes[i % len(complexTypes)]
             figure = new_figure(self.db, figure_type, game)
             deck.append(figure)
+        
         
         # Barajo el mazo
         random.shuffle(deck)
 
         # Asigno las figuras a los jugadores
-        for i in range(len(game.players)):
-            player = game.players[i]
-            for j in range(23):
-                figure = deck.pop()
+        for j in range(23):
+            for i in range(len(game.players)):
+                player = game.players[i]
+                figure = random.choice(deck)
+                deck.remove(figure)
                 put_asign_figure(self.db, figure, player)
+        
         
         # Asigno las figuras que van a iniciar en juego
         for i in range(len(game.players)):
             player = game.players[i]
-            for j in range(3):  # Cantidad de figuras sobre la mesa (no me acuerdo cuantas son)
-                figure = player.figures[j]
+            # Selecciona 3 figuras únicas del jugador
+            selected_figures = random.sample(player.figures, 3)
+            for figure in selected_figures:
                 put_status_figure(self.db, figure, FigureStatus.INHAND)
-
+                
     def get_figures(self, game_id: int):
         game = get_game(self.db, game_id)
         if not game:
