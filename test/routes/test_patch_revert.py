abad6ddeaba8cf051d8ce07dd3d7c76143640d36
Yung_Threesix <aaron.aravena@mi.unc.edu.ar>
2024-10-17 01:32:43 -0300

    fix test

--- test/routes/test_patch_revert.py
+++ test/routes/test_patch_revert.py
@@ -4,27 +4,28 @@ from app.main import app
 from app.schemas.game import RevertRequest
 
 class TestRevertMoves:
+    @pytest.mark.asyncio
     @pytest.mark.parametrize("game_id, revert_request, service_return, expected_status, expected_response", [
         # Caso normal
         (1,
          RevertRequest(player_id=1),
-         {"message": "Turn reverted successfully"},
+         None,  # No se devuelve nada de GameService.revert_moves
          200,
          {"message": "Turn reverted successfully"}
         ),
         # Caso existencial
         (2,
          RevertRequest(player_id=1),
-         Exception("Partida no encontrada"),
+         Exception("No hay cambios para revertir"),
          404,
-         {"detail": "Partida no encontrada"}
+         {"detail": "No hay cambios para revertir"}
          ),
          # Caso excepcional
         (3,
          RevertRequest(player_id=1),
-         Exception("Jugador no encontrado"),
+         Exception("No tienes autorización para revertir estos cambios"),
          401,
-         {"detail": "Jugador no encontrado"}
+         {"detail": "No tienes autorización para revertir estos cambios"}
          ),
          # Caso error server
         (4,
@@ -34,17 +35,18 @@ class TestRevertMoves:
          {"detail": "Internal server error"}
         )
     ])
-    def test_revert_moves(self, mocker, game_id, 
+    async def test_revert_moves(self, mocker, game_id, 
                                revert_request, service_return, 
                                expected_status, expected_response):
         # Cliente
         client = TestClient(app)
         
-        mock_revert_moves = mocker.patch("app.routes.game.GameService.revert_moves")
-        if isinstance(service_return, Exception):
-            mock_revert_moves.side_effect = service_return
-        else:
-            mock_revert_moves.return_value = service_return
-        response = client.patch(f"/games/{game_id}/revert-moves", json=revert_request.model_dump())
+        async def mock_revert_moves(*args, **kwargs):
+                    if isinstance(service_return, Exception):
+                        raise service_return
+                    return None
+        mocker.patch("app.routes.game.GameService.revert_moves", side_effect=mock_revert_moves)
+        
+        response = client.patch(f"games/{game_id}/revert-moves", json=revert_request.model_dump())
         assert response.status_code == expected_status
         assert response.json() == expected_response
