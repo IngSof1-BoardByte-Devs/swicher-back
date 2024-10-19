003dd3428528ec95bb4554c8c9013e7060785133
Yung_Threesix <aaron.aravena@mi.unc.edu.ar>
2024-10-19 08:44:03 -0300

    Schm

--- app/schemas/figure.py
+++ app/schemas/figure.py
@@ -4,3 +4,10 @@ class FigureOut(BaseModel):
     player_id: int
     id_figure: int
     type_figure: str
+
+class FigUpdate(BaseModel):
+    id: int
+    id_player: int
+    type: str  # Tipo de figura (Type 1, Type 2, ..., Type 25)
+    discarded: bool
+    blocked: bool
