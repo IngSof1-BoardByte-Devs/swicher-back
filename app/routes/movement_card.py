23e84d322512fe4353b5222b3d2be6f9faf5944e
Yung_Threesix <aaron.aravena@mi.unc.edu.ar>
2024-11-02 09:03:03 -0300

    movment_card.py

--- app/routes/movement_card.py
+++ app/routes/movement_card.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.schemas.movement import Movement, MovementPartial
+from app.schemas.movement import MovementPartial
 from app.services.movement import MoveService
 from app.database.session import get_db
 from sqlalchemy.orm import Session
@@ -10,22 +10,20 @@ import logging
 router = APIRouter()
 
 
-@router.patch("/{card_id}/", response_model=Movement, tags=["In Game"])
+@router.patch("/{id}/status", tags=["In Game"])
 async def use_movement_card(card_id: int, movement_request: MovementPartial, db: Session = Depends(get_db)):
 
     move_service = MoveService(db)
     try:
-        move = await move_service.set_parcial_movement(movement_request.playerId, card_id,
+        await move_service.set_parcial_movement(movement_request.playerId, card_id,
                                                  movement_request.index1,movement_request.index2)
         
-        return move 
+        return {"msg" : "Carta usada con éxito"} 
     except Exception as e:
         logging.error(f"Error updating movement card: {str(e)}")
         if str(e) == "La carta de movimiento no existe":
             raise HTTPException(status_code=404, detail=str(e))
-        elif str(e) == "La carta no te pertenece":
-            raise HTTPException(status_code=401, detail=str(e))
-        elif str(e) == "La carta no es válida para ese movimiento":
+        elif str(e) == "La carta no te pertenece" or str(e) == "La carta no es válida para ese movimiento": 
             raise HTTPException(status_code=401, detail=str(e))
         else:
             raise HTTPException(status_code=500, detail="Internal server error")
