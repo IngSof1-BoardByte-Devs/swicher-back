496a13aa3db99b91e26ec689d45a6d6cdaf03330
DanielaCourel <daniela.courel@mi.unc.edu.ar>
2024-11-10 16:44:17 -0300

    ING-208 reconección de webscoket

--- app/core/websocket.py
+++ app/core/websocket.py
@@ -6,7 +6,7 @@ manager = ConnectionManager()
 async def websocket_handler(websocket: WebSocket):
     # Conectar al grupo 0 por defecto
     await websocket.accept()
-    await manager.connect(websocket, 0)
+    await manager.connect(websocket, 0, 0)
     print("Se entró por primera vez al websocket_handler la conexión " + str(websocket))
     try:
         while True:
@@ -14,18 +14,39 @@ async def websocket_handler(websocket: WebSocket):
 
             if data.startswith("/join "):
                 print("Se recibió un mensaje de unirse a la partida de " + str(websocket))
-                # Desconectar del grupo actual y conectar al nuevo
-                game_id = int(data.split(" ", 1)[1])
-                manager.move(websocket, 0, game_id)
+                try:
+                    parts = data.split(" ")
+                    if len(parts) >= 3:
+                        game_id = int(parts[1])
+                        player_id = int(parts[2])
+                        manager.move(websocket, 0, game_id, player_id)
+                    else:
+                        print("Error: mensaje de unirse a la partida no tiene suficientes partes")
+                except ValueError as e:
+                    print(f"Error al convertir el mensaje a entero: {data}")
+                    print(e)
 
             elif data.startswith("/leave"):
                 print("Se recibió un mensaje para volver al home de " + str(websocket))
-                # Desconectar del grupo actual y conectar a la sala 0
-                game_id = int(data.split(" ", 1)[1])
-                manager.move(websocket, game_id, 0)
+                try:
+                    parts = data.split(" ")
+                    if len(parts) >= 2:
+                        game_id = int(parts[1])
+                        manager.move(websocket, game_id, 0, None)
+                    else:
+                        print("Error: mensaje de dejar la partida no tiene suficientes partes")
+                except ValueError as e:
+                    print(f"Error al convertir el mensaje a entero: {data}")
+                    print(e)
+
+            elif data.startswith("/rejoin"):
+                print("Se recibió un mensaje para volver a unirse a la partida de " + str(websocket))
+                # Buscar el grupo al que pertenecía y volver a unirse
+                player_id = int(data.split(" ", 1)[1])
+                await manager.reconnect(websocket, player_id)
 
             else:
-                await manager.broadcast(data, 0)
+                await websocket.send_text(data)
     except Exception as e:
         print(e)
     finally:
--- app/websocket_manager.py
+++ app/websocket_manager.py
@@ -3,40 +3,53 @@ from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.groups: Dict[int, List[WebSocket]] = {}
+        self.groups: Dict[int, List[List[WebSocket, int]]] = {}
 
-    async def connect(self, websocket: WebSocket, group: int):
+    async def connect(self, websocket: WebSocket, group: int, id: int):
         """
         Añade a un usuario al grupo indicado. Si ya está en otro grupo, lo desconecta de ese grupo.
         """
-        # Aceptar la conexión
-        print(str(websocket) + "Se ha conectado al grupo " + str(group))
+        print(str(websocket) + " se ha conectado al grupo " + str(group))
         if group not in self.groups:
             self.groups[group] = []
-        self.groups[group].append(websocket)
+        self.groups[group].append([websocket, id])
 
-    def move(self, websocket: WebSocket, old_group: int, new_group: int):
+    def set_id(self, websocket: WebSocket, id: int):
+        """
+        Asigna un id a la conexión.
+        """
+        for group in self.groups:
+            for connection in self.groups[group]:
+                if connection[0] == websocket:
+                    connection[1] = id
+                    print(str(websocket) + " se le asignó el id " + str(id))
+
+    def move(self, websocket: WebSocket, old_group: int, new_group: int, id: int):
         """
         Mueve al usuario del grupo antiguo al nuevo grupo.
         """
         if old_group in self.groups:
-            self.groups[old_group].remove(websocket)
+            for connection in self.groups[old_group]:
+                if connection[0] == websocket:
+                    self.groups[old_group].remove(connection)
+                    break
         if new_group not in self.groups:
             self.groups[new_group] = []
-        self.groups[new_group].append(websocket)
-        print(str(websocket) + "se movió del grupo " + str(old_group) + " al grupo " + str(new_group))
+        self.groups[new_group].append([websocket, id])
+        print(str(websocket) + " se movió del grupo " + str(old_group) + " al grupo " + str(new_group))
 
     async def disconnect(self, websocket: WebSocket):
         """
-        Elimina al usuario del grupo en el que esté actualmente.
+        Marca como None la conexión del websocket. Si no hay más conexiones en el grupo, elimina el grupo.
         """
-        for group in self.groups:
-            if websocket in self.groups[group]:
-                self.groups[group].remove(websocket)
-                print(str(websocket) + "se borró del grupo " + str(group))
-                # Si el grupo está vacío, eliminarlo
-                if len(self.groups[group]) == 0:
-                    del self.groups[group]
+        for group in list(self.groups.keys()):
+            for connection in self.groups[group]:
+                if connection[0] == websocket:
+                    connection[0] = None
+                    break
+            
+            if all(conn[0] is None for conn in self.groups[group]):
+                del self.groups[group]
 
     async def broadcast(self, message: str, group: int):
         """
@@ -44,5 +57,16 @@ class ConnectionManager:
         """
         connections = self.groups.get(group, [])
         for connection in connections:
-            if connection is not None:
-                await connection.send_text(message)
+            if connection[0] is not None:
+                await connection[0].send_text(message)
+
+    async def reconnect(self, websocket: WebSocket, id: int):
+        """
+        Reconecta a un usuario al grupo indicado.
+        """
+        for group in self.groups:
+            for connection in self.groups[group]:
+                if connection[1] == id:
+                    connection[0] = websocket
+                    print(str(websocket) + " se reconectó al grupo " + str(group))
+                    break
